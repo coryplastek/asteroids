e998cecce8ed6fbcc63e138df7a0165659a37aa5
Cory Plastek <cory@coryplastek.com>
2025-07-20 05:55:37 -0500

    feat: max of 60 fps

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
        for event in pygame.event.get():
@@ -18,6 +20,9 @@ def main():
 
 
        pygame.display.flip()
+
+       # wait 1/60s between ticks
+       dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
