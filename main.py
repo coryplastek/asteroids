fdb618372ee3b2e6c7ae399e3023646d81603df8
Cory Plastek <cory@coryplastek.com>
2025-07-28 06:50:17 -0500

    just a happy little accident

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -10,14 +11,15 @@ def main():
     surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return  
        
-       surface.fill('black') 
-
+       surface.fill('black')
+       player.draw(screen)
 
        pygame.display.flip()
 
