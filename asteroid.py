791d7fd1e261def0f7639d8e0acdb54d2d5230ee
Cory Plastek <cory@coryplastek.com>
2025-08-14 22:41:57 -0500

    exploding asteroids!

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+from constants import *
 from circleshape import CircleShape
 
 class Asteroid(CircleShape):
@@ -10,3 +12,19 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            # small asteroids just die
+            return
+        else:
+            # larger asteroids should split into two asteroids, get smaller, faster, and meaner
+            new_angle = random.uniform(20, 50)
+            one = self.velocity.rotate(new_angle)
+            two = self.velocity.rotate(-new_angle)
+
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            for new_asteroid in one, two:
+                asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+                asteroid.velocity = new_asteroid * 1.2
