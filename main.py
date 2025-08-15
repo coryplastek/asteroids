4858108a199ed313016d30fcfa01dffd926b2cf3
Cory Plastek <cory@coryplastek.com>
2025-08-14 22:18:24 -0500

    you have a gun IN SPACE?!

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -13,6 +17,14 @@ def main():
 
     updatable = pygame.sprite.Group() 
     drawable = pygame.sprite.Group() 
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
     Player.containers = (updatable, drawable)
 
     dt = 0
@@ -25,6 +37,11 @@ def main():
        
        updatable.update(dt)
        
+       for asteroid in asteroids:
+           if asteroid.collides_with(player):
+               print("Game over!")
+               sys.exit(0)
+
        screen.fill('black')
        for object in drawable:
            object.draw(screen)
